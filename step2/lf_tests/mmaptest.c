dc5977b7e45f569a723e6a9be2529afc02f26633
John Byrne <john.l.byrne@hpe.com>
2020-02-26 17:23:31 -0700

    Add a threads argument to mmaptest
    This makes it easier to test for multi-threaded lockups.
    Synchronization between client and server now done by socket.
    XDM traffic can be added via separate job.
    
    Signed-off-by: John Byrne <john.l.byrne@hpe.com>

--- step2/lf_tests/mmaptest.c
+++ step2/lf_tests/mmaptest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Hewlett Packard Enterprise Development LP.
+ * Copyright (C) 2019-2020 Hewlett Packard Enterprise Development LP.
  * All rights reserved.
  *
  * This software is available to you under a choice of one of two
@@ -59,6 +59,7 @@ static int              timeout = TIMEOUT;
 
 struct cli_wire_msg {
     uint64_t            mmap_len;
+    uint64_t            threads;
     bool                once_mode;
 };
 
@@ -70,7 +71,15 @@ struct args {
     const char          *node;
     const char          *service;
     uint64_t            mmap_len;
+    uint64_t            threads;
     bool                once_mode;
+};
+
+struct thread_data {
+    struct stuff        *conn;
+    pthread_t           thread;
+    uint64_t            tidx;
+    int                 status;
 };
 
 struct stuff {
@@ -85,6 +94,26 @@ struct stuff {
     struct fi_zhpe_mmap_desc *mdesc;
     bool                allocated;
 };
+
+static pthread_mutex_t  thread_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t   thread_cond0 = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t   thread_cond = PTHREAD_COND_INITIALIZER;
+uint64_t                thread_count;
+static int              thread_state;
+
+enum {
+        STATE_WAIT1,
+        STATE_WAIT2,
+        STATE_DONE,
+};
+
+static int update_error(int old, int new)
+{
+    if (new < 0 && old >= 0)
+        old = new;
+
+    return old;
+}
 
 static void stuff_free(struct stuff *stuff)
 {
@@ -115,7 +144,7 @@ static int do_mem_setup(struct stuff *conn)
     int                 ret = -EEXIST;
     struct fab_conn     *fab_conn = &conn->fab_conn;
     const struct args   *args = conn->args;
-    size_t              req = page_up(args->mmap_len + page_size);
+    size_t              req = args->mmap_len * args->threads + page_size;
 
     ret = fab_mrmem_alloc(fab_conn, &fab_conn->mrmem, req, 0);
     if (ret < 0)
@@ -150,104 +179,101 @@ static int do_mem_xchg(struct stuff *conn, bool client)
     return ret;
 }
 
-static ssize_t do_progress(struct fab_conn *fab_conn,
-                           size_t *tx_cmp, size_t *rx_cmp)
-{
-    ssize_t             ret = 0;
-    ssize_t             rc;
-
-    /* Check both tx and rx sides to make progress.
-     * FIXME: Should rx be necessary for one-sided?
-     */
-    rc = fab_completions(fab_conn->tx_cq, 0, NULL, NULL);
-    if (rc >= 0) {
-        if (tx_cmp)
-            *tx_cmp += rc;
-        else
-            assert(!rc);
-    } else if (ret >= 0)
-        ret = rc;
-
-    rc = fab_completions(fab_conn->rx_cq, 0, NULL, NULL);
-    if (rc >= 0) {
-        if (rx_cmp)
-            *rx_cmp += rc;
-        else
-            assert(!rc);
-    } else if (ret >= 0)
-        ret = rc;
-
-    return ret;
-}
-
-static int do_server_op(struct stuff *conn)
+static void *do_server_thread(void *targ)
 {
     int                 ret = 0;
+    struct thread_data  *thread = targ;
+    struct stuff        *conn = thread->conn;
     struct fab_conn     *fab_conn = &conn->fab_conn;
     const struct args   *args = conn->args;
-    void                *buf = (char *)fab_conn->mrmem.mem + page_size;
-    struct fi_context2  ctx;
-    size_t              tx_avail;
-    size_t              rx_avail;
+    void                *buf = ((char *)fab_conn->mrmem.mem + page_size +
+                                args->mmap_len * thread->tidx);
     uint16_t            *p;
     size_t              i;
 
-    /* Wait for ramp. */
-    ret = fi_recv(fab_conn->ep, NULL, 0, NULL, FI_ADDR_UNSPEC, &ctx);
-    if (ret < 0) {
-        print_func_fi_err(__func__, __LINE__, "fi_recv", "", ret);
-        goto done;
-    }
-    for (rx_avail = 0; !rx_avail;) {
-        ret = do_progress(fab_conn, NULL, &rx_avail);
+    /* Only one thread synchronizes across the wire. */
+    mutex_lock(&thread_mutex);
+    thread_count++;
+    if (thread->tidx) {
+        if (thread_count == args->threads)
+            cond_broadcast(&thread_cond0);
+        while (thread_state == STATE_WAIT1)
+            cond_wait(&thread_cond, &thread_mutex);
+    } else if (thread_count != args->threads)
+            cond_wait(&thread_cond0, &thread_mutex);
+    thread_count--;
+    mutex_unlock(&thread_mutex);
+
+    if (!thread->tidx) {
+        /* Wait for ramps. */
+        ret = sock_recv_fixed_blob(conn->sock_fd, NULL, 0);
         if (ret < 0)
             goto done;
+
+        /* Let the threads run. */
+        mutex_lock(&thread_mutex);
+        thread_state = STATE_WAIT2;
+        mutex_unlock(&thread_mutex);
+        cond_broadcast(&thread_cond);
     }
-    /* Eliminate any  prefetched cache lines. */
-    conn->ext_ops->commit(NULL, buf, args->mmap_len, true, true, true);
+
+    /* Invalidate any prefetched cache lines. */
+    conn->ext_ops->commit(NULL, buf, args->mmap_len, true, true, false);
     /* Compare ramp. */
     for (i = 0, p =  buf; i < args->mmap_len; i += sizeof(*p), p++) {
         if (*p != (typeof(*p))(i | 1))
-            print_err("off 0x%08lx saw 0x%04x\n", i, *p);
+            print_err("t %3" PRIu64 " svr off 0x%08lx saw 0x%04x\n",
+                      thread->tidx, i, *p);
     }
     /* Rewrite ramp. */
     for (i = 0, p = buf; i < args->mmap_len; i+= sizeof(*p), p++)
         *p = i;
-    /* Tell client ramp ready. */
-    ret = fi_send(fab_conn->ep, NULL, 0, NULL, conn->dest_av, &ctx);
-    if (ret < 0) {
-        print_func_fi_err(__func__, __LINE__, "fi_send", "", ret);
-        goto done;
-    }
-    for (tx_avail = 0; !tx_avail;) {
-            ret = do_progress(fab_conn, &tx_avail, NULL);
-            if (ret < 0)
-                goto done;
-    }
-    /* Do a send-receive for the final handshake. */
-    ret = fi_recv(fab_conn->ep, NULL, 0, NULL, FI_ADDR_UNSPEC, &ctx);
-    if (ret < 0) {
-        print_func_fi_err(__func__, __LINE__, "fi_recv", "", ret);
-        goto done;
-    }
-    for (rx_avail = 0; !rx_avail;) {
-        ret = do_progress(fab_conn, NULL, &rx_avail);
+
+    /* Only one thread synchronizes across the wire. */
+    mutex_lock(&thread_mutex);
+    thread_count++;
+    if (thread->tidx) {
+        if (thread_count == args->threads)
+            cond_broadcast(&thread_cond0);
+        while (thread_state == STATE_WAIT2)
+            cond_wait(&thread_cond, &thread_mutex);
+    } else if (thread_count != args->threads)
+            cond_wait(&thread_cond0, &thread_mutex);
+    thread_count--;
+    mutex_unlock(&thread_mutex);
+
+    if (!thread->tidx) {
+        /* Tell client ramps are ready. */
+        ret = sock_send_blob(conn->sock_fd, NULL, 0);
         if (ret < 0)
             goto done;
+
+        /* Final handshake. */
+        ret = sock_recv_fixed_blob(conn->sock_fd, NULL, 0);
+        if (ret < 0)
+            goto done;
+
+        /* Let the threads run. */
+        mutex_lock(&thread_mutex);
+        thread_state = STATE_DONE;
+        mutex_unlock(&thread_mutex);
+        cond_broadcast(&thread_cond);
     }
 
  done:
-    return ret;
+    thread->status = ret;
+
+    return NULL;
 }
 
-static int do_client_op(struct stuff *conn)
+static void *do_client_thread(void *targ)
 {
     int                 ret = 0;
-    struct fab_conn     *fab_conn = &conn->fab_conn;
+    struct thread_data  *thread = targ;
+    struct stuff        *conn = thread->conn;
     const struct args   *args = conn->args;
-    struct fi_context2  ctx;
-    size_t              tx_avail;
-    size_t              rx_avail;
+    void                *buf = ((char *)conn->mdesc->addr +
+                                args->mmap_len * thread->tidx);
     uint16_t            *p;
     size_t              i;
     uint64_t            lat_write;
@@ -259,13 +285,15 @@ static int do_client_op(struct stuff *conn)
 
     /* Write ramp. */
     start = get_cycles(NULL);
-    for (i = 0, p = conn->mdesc->addr; i < args->mmap_len; i += sizeof(*p), p++)
+    for (i = 0, p = buf; i < args->mmap_len; i += sizeof(*p), p++)
         *p = (i | 1);
     now = get_cycles(NULL);
     lat_write = now - start;
+
     /* Commit buffer. */
     start = now;
-    ret = conn->ext_ops->commit(conn->mdesc, 0, 0, true, false, true);
+    ret = conn->ext_ops->commit(conn->mdesc, buf, args->mmap_len,
+                                true, false, true);
     if (ret < 0) {
         print_func_fi_err(__func__, __LINE__, "ext_commit", "", ret);
         goto done;
@@ -273,66 +301,92 @@ static int do_client_op(struct stuff *conn)
     now = get_cycles(NULL);
     lat_commit = now - start;
 
-    /* Tell server ramp is ready. */
-    ret = fi_send(fab_conn->ep, NULL, 0, NULL, conn->dest_av, &ctx);
-    if (ret < 0) {
-        print_func_fi_err(__func__, __LINE__, "fi_send", "", ret);
-        goto done;
-    }
-    for (tx_avail = 0; !tx_avail;) {
-            ret = do_progress(fab_conn, &tx_avail, NULL);
-            if (ret < 0)
-                goto done;
-    }
-    /* Wait for ramp ready. */
-    ret = fi_recv(fab_conn->ep, NULL, 0, NULL, FI_ADDR_UNSPEC, &ctx);
-    if (ret < 0) {
-        print_func_fi_err(__func__, __LINE__, "fi_recv", "", ret);
-        goto done;
-    }
-    for (rx_avail = 0; !rx_avail;) {
-        ret = do_progress(fab_conn, NULL, &rx_avail);
+    /* Only one thread synchronizes across the wire. */
+    mutex_lock(&thread_mutex);
+    thread_count++;
+    if (thread->tidx) {
+        if (thread_count == args->threads)
+            cond_broadcast(&thread_cond0);
+        while (thread_state == STATE_WAIT1)
+            cond_wait(&thread_cond, &thread_mutex);
+    } else if (thread_count != args->threads)
+            cond_wait(&thread_cond0, &thread_mutex);
+    thread_count--;
+    mutex_unlock(&thread_mutex);
+
+    if (!thread->tidx) {
+        /* Tell server ramps are ready. */
+        ret = sock_send_blob(conn->sock_fd, NULL, 0);
         if (ret < 0)
             goto done;
+
+        /* Wait for ramps. */
+        ret = sock_recv_fixed_blob(conn->sock_fd, NULL, 0);
+        if (ret < 0)
+            goto done;
+
+        /* Let the threads run. */
+        mutex_lock(&thread_mutex);
+        thread_state = STATE_WAIT2;
+        mutex_unlock(&thread_mutex);
+        cond_broadcast(&thread_cond);
     }
 
-    /* Flush buffer. */
+    /* Invalidate any prefetched cache lines. */
     start = get_cycles(NULL);
-    conn->ext_ops->commit(conn->mdesc, NULL, 0, true, true, true);
+    conn->ext_ops->commit(conn->mdesc, buf, args->mmap_len, true, true, false);
     now = get_cycles(NULL);
     lat_flush = now - start;
+
     /* Compare ramp. */
     start = now;
     for (i = 0, p = conn->mdesc->addr; i < args->mmap_len;
          i += sizeof(*p), p++) {
         if (*p != (typeof(*p))i)
-            print_err("off 0x%08lx saw 0x%04x\n", i, *p);
+            print_err("t %3" PRIu64 " cli off 0x%08lx saw 0x%04x\n",
+                      thread->tidx, i, *p);
     }
     now = get_cycles(NULL);
     lat_read = now - start;
 
-    /* Do a send-receive for the final handshake. */
-    ret = fi_send(fab_conn->ep, NULL, 0, NULL, conn->dest_av, &ctx);
-    if (ret < 0) {
-        print_func_fi_err(__func__, __LINE__, "fi_send", "", ret);
-        goto done;
+    /* Only one thread synchronizes across the wire. */
+    mutex_lock(&thread_mutex);
+    thread_count++;
+    if (thread->tidx) {
+        if (thread_count == args->threads)
+            cond_broadcast(&thread_cond0);
+        while (thread_state == STATE_WAIT2)
+            cond_wait(&thread_cond, &thread_mutex);
+    } else if (thread_count != args->threads)
+            cond_wait(&thread_cond0, &thread_mutex);
+    thread_count--;
+    mutex_unlock(&thread_mutex);
+
+    if (!thread->tidx) {
+        /* Final handshake. */
+        ret = sock_send_blob(conn->sock_fd, NULL, 0);
+        if (ret < 0)
+            goto done;
+
+        /* Let the threads run. */
+        mutex_lock(&thread_mutex);
+        thread_state = STATE_DONE;
+        mutex_unlock(&thread_mutex);
+        cond_broadcast(&thread_cond);
     }
-    for (tx_avail = 0; !tx_avail;) {
-            ret = do_progress(fab_conn, &tx_avail, NULL);
-            if (ret < 0)
-                goto done;
-    }
-    fab_print_info(fab_conn);
-    printf("%s:mmap_len 0x%lx lat write/commit/flush/read"
+
+    printf("%s:t %" PRIu64 " mmap_len 0x%lx lat write/commit/flush/read"
            " %.3lf/%.3lf/%.3lf/%.3lf\n",
-           appname, args->mmap_len,
+           appname, thread->tidx, args->mmap_len,
            cycles_to_usec(lat_write, 1),
            cycles_to_usec(lat_commit, 1),
            cycles_to_usec(lat_flush, 1),
            cycles_to_usec(lat_read, 1));
 
  done:
-    return ret;
+    thread->status = ret;
+
+    return NULL;
 }
 
 static int do_server_one(const struct args *oargs, int conn_fd)
@@ -348,7 +402,11 @@ static int do_server_one(const struct args *oargs, int conn_fd)
     struct fab_dom      *fab_dom = &conn.fab_dom;
     struct fab_conn     *fab_conn = &conn.fab_conn;
     struct fab_conn     *fab_listener = &conn.fab_listener;
+    struct thread_data  *threads = NULL;
     struct cli_wire_msg cli_msg;
+    size_t              i;
+    void                *retval;
+    int                 rc;
 
     fab_dom_init(fab_dom);
     fab_conn_init(fab_dom, fab_conn);
@@ -360,6 +418,7 @@ static int do_server_one(const struct args *oargs, int conn_fd)
         goto done;
 
     args->mmap_len = be64toh(cli_msg.mmap_len);
+    args->threads = be64toh(cli_msg.threads);
     args->once_mode = !!cli_msg.once_mode;
 
     ret = fab_dom_setup(NULL, NULL, true, PROVIDER, NULL, EP_TYPE, fab_dom);
@@ -389,7 +448,40 @@ static int do_server_one(const struct args *oargs, int conn_fd)
     if (ret < 0)
         goto done;
 
-    ret = do_server_op(&conn);
+    threads = calloc(args->threads, sizeof(*threads));
+    if (!threads) {
+        ret = - ENOMEM;
+        goto done;
+    }
+
+    ret = 0;
+    thread_state = STATE_WAIT1;
+
+    for (i = 0; i < args->threads; i++) {
+        threads[i].conn = &conn;
+        threads[i].tidx = i;
+        ret = -pthread_create(&threads[i].thread, NULL, do_server_thread,
+                              &threads[i]);
+        if (ret < 0) {
+            print_func_err(__func__, __LINE__, "pthread_create", "", ret);
+            break;
+        }
+    }
+    for (i = 0; i < args->threads; i++) {
+        if (!threads[i].conn)
+            break;
+        if (ret < 0)
+            (void)pthread_cancel(threads[i].thread);
+        rc = -pthread_join(threads[i].thread, &retval);
+        ret = update_error(ret, rc);
+        if (rc < 0)
+            continue;
+        if (retval == PTHREAD_CANCELED) {
+            ret = update_error(ret, -EINTR);
+            continue;
+        }
+        ret = update_error(ret, threads[i].status);
+    }
 
  done:
     stuff_free(&conn);
@@ -466,7 +558,11 @@ static int do_client(const struct args *args)
     struct fab_dom      *fab_dom = &conn.fab_dom;
     struct fab_conn     *fab_conn = &conn.fab_conn;
     struct fab_conn     *fab_listener = &conn.fab_listener;
+    struct thread_data  *threads = NULL;
     struct cli_wire_msg cli_msg;
+    size_t              i;
+    void                *retval;
+    int                 rc;
 
     fab_dom_init(fab_dom);
     fab_conn_init(fab_dom, fab_conn);
@@ -479,6 +575,7 @@ static int do_client(const struct args *args)
 
     /* Write the ring parameters to the server. */
     cli_msg.mmap_len = htobe64(args->mmap_len);
+    cli_msg.threads = htobe64(args->threads);
     cli_msg.once_mode = args->once_mode;
 
     ret = sock_send_blob(conn.sock_fd, &cli_msg, sizeof(cli_msg));
@@ -509,7 +606,8 @@ static int do_client(const struct args *args)
     if (ret < 0)
         goto done;
 
-    ret = conn.ext_ops->mmap(NULL, args->mmap_len, PROT_READ | PROT_WRITE,
+    ret = conn.ext_ops->mmap(NULL, args->mmap_len * args->threads,
+                             PROT_READ | PROT_WRITE,
                              MAP_SHARED, page_size, fab_conn->ep, conn.dest_av,
                              conn.remote_key, FI_ZHPE_MMAP_CACHE_WB,
                              &conn.mdesc);
@@ -519,7 +617,40 @@ static int do_client(const struct args *args)
     }
 
     /* Write/read mmap region  */
-    ret = do_client_op(&conn);
+    threads = calloc(args->threads, sizeof(*threads));
+    if (!threads) {
+        ret = - ENOMEM;
+        goto done;
+    }
+
+    ret = 0;
+    thread_state = STATE_WAIT1;
+
+    for (i = 0; i < args->threads; i++) {
+        threads[i].conn = &conn;
+        threads[i].tidx = i;
+        ret = -pthread_create(&threads[i].thread, NULL, do_client_thread,
+                              &threads[i]);
+        if (ret < 0) {
+            print_func_err(__func__, __LINE__, "pthread_create", "", ret);
+            break;
+        }
+    }
+    for (i = 0; i < args->threads; i++) {
+        if (!threads[i].conn)
+            break;
+        if (ret < 0)
+            (void)pthread_cancel(threads[i].thread);
+        rc = -pthread_join(threads[i].thread, &retval);
+        ret = update_error(ret, rc);
+        if (rc < 0)
+            continue;
+        if (retval == PTHREAD_CANCELED) {
+            ret = update_error(ret, -EINTR);
+            continue;
+        }
+        ret = update_error(ret, threads[i].status);
+    }
 
  done:
     stuff_free(&conn);
@@ -533,13 +664,15 @@ static void usage(bool help)
 {
     print_usage(
         help,
-        "Usage:%s [-o] <port> [<node> <mmap_len>]\n"
+        "Usage:%s [-o] [-t threads] <port> [<node> <mmap_len>]\n"
         "All sizes may be postfixed with [kmgtKMGT] to specify the"
         " base units.\n"
         "Lower case is base 10; upper case is base 2.\n"
         "Server requires just port; client requires all 3 arguments.\n"
         "Client only options:\n"
-        " -o : run once and then server will exit\n",
+        " -o : run once and then server will exit\n"
+        " -t <threads> : number of simultaneous threads,"
+        " <mmap_len> buffer for each\n",
         appname);
 
     exit(help ? 0 : 255);
@@ -558,7 +691,7 @@ int main(int argc, char **argv)
     if (argc == 1)
         usage(true);
 
-    while ((opt = getopt(argc, argv, "o")) != -1) {
+    while ((opt = getopt(argc, argv, "ot:")) != -1) {
 
         /* All opts are client only, now. */
         client_opt = true;
@@ -571,11 +704,24 @@ int main(int argc, char **argv)
             args.once_mode = true;
             break;
 
+        case 't':
+            if (args.threads)
+                usage(false);
+            if (parse_kb_uint64_t(__func__, __LINE__, "threads",
+                                  optarg, &args.threads, 0,
+                                  1, SIZE_MAX, PARSE_KB | PARSE_KIB) < 0)
+                usage(false);
+            break;
+
         default:
             usage(false);
 
         }
     }
+
+    if (!args.threads)
+        args.threads = 1;
+    args.mmap_len = page_up(args.mmap_len + page_size);
 
     opt = argc - optind;
 
@@ -591,7 +737,7 @@ int main(int argc, char **argv)
         if (parse_kb_uint64_t(__func__, __LINE__, "mmap_len",
                               argv[optind++], &args.mmap_len, 0,
                               sizeof(uint16_t), SIZE_MAX,
-                              PARSE_KB | PARSE_KIB))
+                              PARSE_KB | PARSE_KIB) < 0)
             usage(false);
         if (do_client(&args) < 0)
             goto done;

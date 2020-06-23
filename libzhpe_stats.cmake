162e8f10e5d745dd95c6860b278a28b4c39a2aaa
John Byrne <john.l.byrne@hpe.com>
2020-06-23 11:31:18 -0600

    Fix nit in libzhpe_stats.cmake
    Signed-off-by: John Byrne <john.l.byrne@hpe.com>

--- libzhpe_stats.cmake
+++ libzhpe_stats.cmake
@@ -3,6 +3,6 @@ set(ZSTA_LIB ${LIBD}/lib/libzhpe_stats.so)
 target_link_libraries(zhpe_stats INTERFACE ${ZSTA_LIB})
 if (ZSTA)
   message("ZSTA_LIB " ${ZSTA_LIB})
-  target_compile_definitions(zhpe_stats INTERFACE -DHAVE_ZHPE_STATS)
+  target_compile_definitions(zhpe_stats INTERFACE HAVE_ZHPE_STATS)
 endif(ZSTA)
 

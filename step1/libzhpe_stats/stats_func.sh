2006d4d07190722e55e40346be0db05158694640
John Byrne <john.l.byrne@hpe.com>
2020-09-07 14:11:38 -0600

    zhpe-stats: add script for handling core-dumps
    Tweaks to stats code to make information easier to get.
    
    Signed-off-by: John Byrne <john.l.byrne@hpe.com>

--- step1/libzhpe_stats/stats_func.sh
+++ step1/libzhpe_stats/stats_func.sh
@@ -2,19 +2,22 @@
 
 set -e
 
-(( $# == 1 )) || exit 1
-[[ -n "$MPIROOT" ]] || exit 1
+(( $# == 2 )) || exit 1
+[[ -n "$MPIROOT" ]] || ( echo MPIROOT missing 2>&1; exit 1 )
+
+STATS_DIR=$1
+PFX=$2
 
 TD=""
 function finish {
     /bin/rm -rf $TD
 }
-TD=$(mktemp -d)
 trap finish EXIT
+TD=$(mktemp -d)
 
-cd $1
-rm -f *.0 *.out
-for F in *; do
+cd $STATS_DIR
+rm -f $PFX.*.0 $PFX.*.out
+for F in $PFX*[0-9] $PFX*.func; do
     B=$F
     B=${B%.func}
     B=${B%.*}
@@ -26,12 +29,30 @@ for F in *; do
     fi
 done
 
+I=0
 for D in $TD/*.data; do
     for F in $(cat $D); do 
-	$MPIROOT/../libexec/zhpe_stats/unpackdata.py $F |
-	    sed $(cat ${D%.data}.func) > $F.out
+	( $MPIROOT/../libexec/zhpe_stats/unpackdata.py $F |
+	    sed $(cat ${D%.data}.func) |
+	    awk -F , '
+	    $1 != "8" { next; }
+	    $2 == "1000000" {
+		for (i = 6; i <= 9; i++)
+		    $i = sprintf("0x%x", $i);
+	    }
+	    $2 == "1000001" {
+		for (i = 4; i <= 9; i++)
+		    $i = sprintf("0x%x", $i);
+	    }
+	    { print $0; }' > $F.out ) &
+	   if (( ++I >= 16 )); then
+	       wait
+	       I=0
+	   fi
     done
 done
+wait
+
 
 
 
